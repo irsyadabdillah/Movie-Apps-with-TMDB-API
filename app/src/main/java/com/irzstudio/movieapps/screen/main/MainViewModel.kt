c83eadc871fc7eb596c3ed54ebf0a56a32f4be85
Irsyad Abdillah <irsyadabdilah96@gmail.com>
2021-05-09 16:17:07 +0700

    checking firebasecloudmessage

--- app/src/main/java/com/irzstudio/movieapps/screen/main/MainViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.irzstudio.movieapps.screen.main
-
-class MainViewModel {
-}
--- app/src/main/java/com/irzstudio/movieapps/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/irzstudio/movieapps/service/MyFirebaseMessagingService.kt
@@ -1,4 +1,33 @@
 package com.irzstudio.movieapps.service
 
-class MyFirebaseMessagingService {
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+
+        val refreshToken = FirebaseInstanceId.getInstance().token
+
+        Log.e("refreshToken", refreshToken!!)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        showNotification(p0.notification?.title.toString(), p0.notification?.body.toString())
+        val checkToken = FirebaseInstanceId.getInstance().token
+        Log.d("checkToken", checkToken!!)
+    }
+
+    fun showNotification(title: String, message: String){
+        val builder = NotificationCompat.Builder(this, "notification")
+            .setContentTitle(title)
+            .setContentText(message)
+        val manager = NotificationManagerCompat.from(this)
+        manager.notify(1, builder.build())
+    }
 }
